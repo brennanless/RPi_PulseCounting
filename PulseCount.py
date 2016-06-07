26db1a0b56e4117d05921f5d11b4d2e9e6b30a1b
BrennanLess <brennan.less@gmail.com>
2016-06-07 13:34:36 -0700

    Added with-statemetns to all file openings, so that keyboard interrupt or other program crashing leads to closure of files.

--- PulseCount.py
+++ PulseCount.py
@@ -56,20 +56,13 @@ historyFile = '/home/pi/Documents/PulseCount/count_data.csv'
 cumFile = '/home/pi/Documents/PulseCount/cum_count.txt'
 shelveFile = '/home/pi/Documents/PulseCount/smap_post.db'
 
-#open shelve file for persistent python object storage
-shelf = shelve.open(shelveFile)
-
-#test if object keys already exist, if not, create them.
-if not shelf.has_key('smap_value'):
-	shelf['smap_value'] = []
-
 #Assign values to pulse_count, diff_pulse and old_count.
 #If cumFile exists, open it for reading. Set pulse_count value based on file value.
 if(os.path.exists(cumFile)==True):
-	cum = open(cumFile, 'r')
-	dat = cum.read()
-	pulse_count = int(dat)	
-	cum.close()
+	with open(cumFile, 'r') as cum:
+		dat = cum.read()
+		pulse_count = int(dat)	
+		#cum.close() #Still needed?
 #If file does not exist, just assign pulse_count to 0. 	
 else:
 	pulse_count = 0
@@ -92,65 +85,52 @@ def main():
 	global diff_pulse
 	global old_count	
 	global pulse_count
+	
+	#open shelve file for persistent python object storage
+	with shelve.open(shelveFile) as shelf:
+		#shelf = shelve.open(shelveFile)
+		#test if object keys already exist, if not, create them.
+		if not shelf.has_key('smap_value'):
+			shelf['smap_value'] = []
 
-	#infinite loop with 60-second delay.
-	while True:
-		#Open data files for writing values.
-		cum = open(cumFile, 'w') #this will overwrite whatever the prior value is.
-		datacsv = open(historyFile, 'a') #this will append without overwriting prior data.
-		#Calculate pulses in current minute
-		diff_pulse = pulse_count - old_count
-		old_count = pulse_count #update old_count
-		DT = datetime.now()
-		TimeStr = DT.strftime('%Y-%m-%d %H:%M:%S')
-		#Write values to files.
-		datacsv.write(TimeStr + ',' + str(pulse_count) + ',' + str(diff_pulse) + '\n')
-		cum.write(str(pulse_count))
-		#Close data files
-		cum.close()
-		datacsv.close()
+		#infinite loop with 60-second delay.
+		while True:
 		
-		print 'Total pulses counted = %i; recent pulses = %i' %(pulse_count, diff_pulse)
+			#Open data files for writing values.
+			with open(cumFile, 'w') as cum, open(historyFile, 'a') as datacsv:
+				#cum = open(cumFile, 'w') #this will overwrite whatever the prior value is.
+				#datacsv = open(historyFile, 'a') #this will append without overwriting prior data.
+				#Calculate pulses in current minute
+				diff_pulse = pulse_count - old_count
+				old_count = pulse_count #update old_count
+				DT = datetime.now()
+				TimeStr = DT.strftime('%Y-%m-%d %H:%M:%S')
+				#Write values to files.
+				datacsv.write(TimeStr + ',' + str(pulse_count) + ',' + str(diff_pulse) + '\n')
+				cum.write(str(pulse_count))
+				#Close data files
+				#cum.close()
+				#datacsv.close()
+		
+			print 'Total pulses counted = %i; recent pulses = %i' %(pulse_count, diff_pulse)
 
-		#Place current values in persistent python object storage using shelve.
-		#If shelve has values in it, they are posted to smap database.
-		#If the post is successful, the values are removed from the object. 
-		#If not successful, values remain for next iteration of main().
-		shelf['smap_value'] += [[time_str_to_ms(TimeStr), diff_pulse]]
+			#Place current values in persistent python object storage using shelve.
+			#If shelve has values in it, they are posted to smap database.
+			#If the post is successful, the values are removed from the object. 
+			#If not successful, values remain for next iteration of main().
+			shelf['smap_value'] += [[time_str_to_ms(TimeStr), diff_pulse]]
 		
-		if shelf['smap_value']:
-			response = smap_post(smap_sourcename, shelf['smap_value'], path, uuid_pulse_diff, units)
-			if not response:
-				shelf['smap_value'] = []
-			#for smap_value in shelf['smap_value']:
-				#response = smap_post(smap_sourcename, smap_value, path, uuid_pulse_diff, units)
-				#if not response:
-					#shelf.remove(smap_value)
-		#I have some concerns about the timing of this, when integrating the smap posting/shelf elements. 
-		#I've tested on my laptop, posting a single value, took 0.0071 seconds, which should not cause much disruption.		
-		time.sleep(60)
+			if shelf['smap_value']:
+				response = smap_post(smap_sourcename, shelf['smap_value'], path, uuid_pulse_diff, units)
+				if not response:
+					shelf['smap_value'] = [] #Empty the list if upload to smap was successful. 
+				#for smap_value in shelf['smap_value']:
+					#response = smap_post(smap_sourcename, smap_value, path, uuid_pulse_diff, units)
+					#if not response:
+						#shelf.remove(smap_value)
+			#I have some concerns about the timing of this, when integrating the smap posting/shelf elements. 
+			#I've tested on my laptop, posting a single value, took 0.0071 seconds, which should not cause much disruption.		
+			time.sleep(60)
 
 if __name__ == "__main__":
 	main()
-	
-# 	
-# 	
-# 	
-# >>> shelf = []
-# >>> shelf += [[time_str_to_ms('2016-06-07 10:00:00'), 20]]
-# >>> shelf
-# [[1465318800000L, 20]]
-# >>> shelf += [[time_str_to_ms('2016-06-07 10:01:00'), 22]]
-# >>> shelf += [[time_str_to_ms('2016-06-07 10:02:00'), 24]]
-# >>> shelf += [[time_str_to_ms('2016-06-07 10:03:00'), 26]]	
-# 
-# smap_post(smap_sourcename, shelf, path, uuid_pulse_diff, units)
-# 
-# if shelf:
-# 	for smap_value in shelf:
-# 		response = smap_post(smap_sourcename, smap_value, path, uuid_pulse_diff, units)
-# 
-# if shelf['smap_value']:
-# 	response = smap_post(smap_sourcename, shelf['smap_value'], path, uuid_pulse_diff, units)
-# 	if not response:
-# 		shelf['smap_value'] = []
