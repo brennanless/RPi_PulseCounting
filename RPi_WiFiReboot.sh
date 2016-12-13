5f24485db21e612cb50635514af547ac77c5a991
brennanless <brennan.less@gmail.com>
2016-12-13 11:31:48 -0800

    Had to change the RPi_reboot script to d oa full reboot, because network ifup if down settings were not working.

--- RPi_WiFiReboot.sh
+++ RPi_WiFiReboot.sh
@@ -7,8 +7,9 @@ ping -c4 192.168.21.1 > /dev/null
  
 if [ $? != 0 ] 
 then
-  echo "No network connection, restarting wlan0"
-  /sbin/ifdown 'wlan0'
-  sleep 5
-  /sbin/ifup --force 'wlan0'
+  sudo /sbin/shutdown -r now
+  #echo "No network connection, restarting wlan0"
+  #sudo /sbin/ifdown 'wlan0'
+  #sleep 5
+  #sudo /sbin/ifup --force 'wlan0'
 fi
