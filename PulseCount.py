0e46c7eb1e4957eef3df2c931983bef5862592c3
BrennanLess <brennan.less@gmail.com>
2017-05-03 09:32:10 -0700

    Try to adjust ln 67 where apparently theres a Non-ASCII character

--- PulseCount.py
+++ PulseCount.py
@@ -65,7 +65,6 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #Interrupt added to GPIO27 pin, execute pulse() whenever falling edge is detected.
 GPIO.add_event_detect(27, GPIO.FALLING, callback=pulse_1, bouncetime=200)
 GPIO.add_event_detect(18, GPIO.FALLING, callback=pulse_2)
-
 def main():
 
 	#instantiate global variables, so function affects values outside of function namespace.
